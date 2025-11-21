fe501a1c41d10d29beea158af96dae01fd694c34
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-21 11:21:20 +0800

    Implement GradeCalculator for UndergraduateStudent

---
+++ src/UndergraduateStudentGradeCalculator.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class UndergraduateStudentGradeCalculator implements GradeCalculator {
+
+    @Override
+    public String getProficiency(List<CourseGrade> grades, double gwa) {
+        if (gwa == 0.0 && grades.isEmpty()) return "N/A";
+
+        if (gwa == 4.0) return "Excellent";
+        else if (gwa == 3.5) return "Superior";
+        else if (gwa == 3.0) return "Very Good";
+        else if (gwa == 2.5) return "Good";
+        else if (gwa == 2.0) return "Satisfactory";
+        else if (gwa == 1.5) return "Fair";
+        else if (gwa == 1.0) return "Pass";
+        else return "Failed";
+    }
+}
