1ef63090a102081d8bd9e7f9424788a75efd1c5d
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-21 13:30:15 +0800

    Move choice logic to GradingSytem and use refactored StudentsFileManager

--- src/main/java/com/xenodirt/Main.java
+++ src/main/java/com/xenodirt/Main.java
@@ -1,81 +1,34 @@
 package com.xenodirt;
 
-import java.io.IOException;
+import tools.jackson.core.JacksonException;
+
+import java.io.File;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static final String DATA_FILE = "students.dat";
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StudentManager manager = new StudentManager();
-        StudentFileManager studentFileManager = new StudentFileManager();
+
+        File studentsFile = FileUtils.getFileBesideJar("students.json");
+        StudentFileManager studentFileManager = new StudentFileManager(studentsFile);
 
         // Load existing students from file
         try {
-            List<Student> loaded = studentFileManager.loadFromFile(DATA_FILE);
+            List<Student> loaded = studentFileManager.loadFromFile();
 
             manager.setStudents(loaded);
 
             System.out.println("Loaded " + loaded.size() + " students from file.");
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (JacksonException e) {
             System.out.println("Could not load existing data: " + e.getMessage());
         }
 
-        int choice;
+        var system = new GradingSystem(sc, manager, studentFileManager);
 
-        do {
-            printMenu();
-
-            choice = getIntInput(sc, "Enter choice: ");
-
-            System.out.println();
-
-            switch (choice) {
-                case 1:
-                    addStudent(sc, manager);
-                    pressEnterToReturn(sc);
-
-                    break;
-                case 2:
-                    addGradeToStudent(sc, manager);
-                    pressEnterToReturn(sc);
-
-                    break;
-                case 3:
-                    viewStudent(sc, manager);
-                    pressEnterToReturn(sc);
-
-                    break;
-                case 4:
-                    manager.listAllStudents();
-                    pressEnterToReturn(sc);
-
-                    break;
-                case 5:
-                    removeStudent(sc, manager);
-                    pressEnterToReturn(sc);
-
-                    break;
-                case 6:
-                    saveData(manager);
-                    pressEnterToReturn(sc);
-
-                    break;
-                case 7:
-                    saveData(manager);
-
-                    System.out.println("Exiting...");
-
-                    break;
-                default:
-                    System.out.println("Invalid choice.");
-            }
-        } while (choice != 0);
+        system.run();
 
         sc.close();
     }
-
-
 }
