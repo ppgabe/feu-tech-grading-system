50accaa4a2722585002d7adcbb05809816cf2e4a
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-21 11:29:17 +0800

    Add GWA and Proficiency to Student toString method

--- src/Student.java
+++ src/Student.java
@@ -2,19 +2,28 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public record Student(String id, String name, StudentType type, List<CourseGrade> courseGrades) implements Serializable {
+public record Student(String id, String name, StudentType type,
+                      List<CourseGrade> courseGrades) implements Serializable {
 
     @Override
     public String toString() {
+        double gwa = type.getGradeCalculator().calculateGWA(courseGrades);
+
         return String.format(
             """
                 ID: %d
                 Name: %s
                 Type: %s
+                -------------
+                GWA: %.2f
+                Proficiency: %s
+                -------------
                 """,
             id,
             name,
-            type
+            type,
+            gwa,
+            type.getGradeCalculator().getProficiency(courseGrades, gwa)
             );
     }
 }
