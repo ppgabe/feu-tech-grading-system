4df961d2d5f582ea9c501cc285c4f1b6629aa674
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-21 11:10:09 +0800

    Remove unnecessary Person class, switch to records
    Further steps to decouple logic from our data classes, CourseGrade and
    Student.

--- src/CourseGrade.java
+++ src/CourseGrade.java
@@ -1,28 +1,6 @@
 import java.io.Serializable;
 
-public class CourseGrade implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String courseName;
-    private int units;
-    private double gradePoint;
-
-    public CourseGrade(String courseName, int units, double gradePoint) {
-        this.courseName = courseName;
-        this.units = units;
-        this.gradePoint = gradePoint;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public int getUnits() {
-        return units;
-    }
-
-    public double getGradePoint() {
-        return gradePoint;
-    }
+public record CourseGrade(String courseName, int units, double gradePoint) implements Serializable {
 
     @Override
     public String toString() {
