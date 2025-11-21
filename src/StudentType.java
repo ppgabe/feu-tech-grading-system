c443b91d0ce53ec68aa2c22576b7813fb2f74be7
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-21 11:23:59 +0800

    Add GradeCalculator instances for each StudentType
    Now, the grade calculation is coupled to the StudentType, not the
    Student data.

--- src/StudentType.java
+++ src/StudentType.java
@@ -1,13 +1,19 @@
 import java.util.List;
 
 public enum StudentType {
-    UNDERGRADUATE("Undergraduate"),
-    GRADUATE("Graduate");
+    UNDERGRADUATE("Undergraduate", new UndergraduateStudentGradeCalculator()),
+    GRADUATE("Graduate", new GraduateStudentGradeCalculator());
 
     private final String formattedName;
+    private final GradeCalculator gradeCalculator;
 
-    private StudentType(String s) {
+    private StudentType(String s, GradeCalculator gc) {
         this.formattedName = s;
+        this.gradeCalculator = gc;
+    }
+
+    public GradeCalculator getGradeCalculator() {
+        return gradeCalculator;
     }
 
     @Override
